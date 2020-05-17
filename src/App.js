109499ef0a421ea0e7d7eefde08887656ce42655
jimmymetts <jimmymetts@bellsouth.net>
2020-05-16 22:36:50 -0500

    added constructor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
+class App extends Component {
+ 
+      constructor(props){
+        super(props);
+
+        this.state={
+          newItem:"",
+
+        }
+      }
+      render() {
+      return (
+        <div className="App">
     </div>
   );
 }
+}
+
 
 export default App;
